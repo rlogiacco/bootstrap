339ed411077d1c3cc1ea685b0b2c0797215716ff
Roberto Lo Giacco <rlogiacco@gmail.com>
2015-01-28 19:16:44 +0100

    added default tooltip inner tag content holder

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -261,7 +261,7 @@ angular.module( 'ui.bootstrap.tooltip', [ 'ui.bootstrap.position', 'ui.bootstrap
              */
             attrs.$observe( type, function ( val ) {
               if (val.charAt(0) == '@') {
-              	var tag = element.find(val.substring(1));
+              	var tag = element.find(val.length > 1 ? val.substring(1) : 'tooltip');
               	if (tag) {
               	  ttScope.content = tag.html();
               	  tag.remove();
